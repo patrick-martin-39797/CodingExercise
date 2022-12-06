a9c143e5ad43692a95c22c4e2dbcc5e60dbbc4e3
Patrick Martin <patrick.martin@nelnet.net>
2022-12-06 16:57:00 -0500

    Initial Commit

---
+++ Sample/SampleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// Complete the solution to make the following code work.   
+
+Cart groceryCart = new Cart() { TypeOfCart = TypeOfCart.Grocery }; ;
+groceryCart.AddItem(new Dairy().WithDescription("Gallon of Milk").WithUnitPrice(3.50m).WithQuantity(2));
+groceryCart.AddItem(new Dairy().WithDescription("Heavy Cream").WithUnitPrice(4.59m));
+groceryCart.AddItem(new Dairy().WithDescription("Yogurt").WithUnitPrice(.59m).WithQuantity(6));
+groceryCart.AddItem(new Meat().WithWeight(3.5m).WithDescription("Hamburger").WithUnitPrice(2.96m));
+
+Console.WriteLine($"{groceryCart.NumberOfItems} items at a total price of ${groceryCart.TotalPrice()}");
+Console.WriteLine($"The {groceryCart.TypeOfCart} Cart contains the following {groceryCart.Contents()}");
+Console.ReadLine();
